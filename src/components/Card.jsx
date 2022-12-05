0a389261801938e19c4d8de6391c702e4c80a049
Rune <rune1833@live.dk>
2022-12-05 11:23:18 +0100

    Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,21 +5,19 @@ export default function Card(props) {
     return (
 
         <> 
-        <div id="carusel">
-        <div className="card">
-            <img 
-                src={props.coverImg}
-                className="card--image" 
-            />
-            <div className="card--stats">
-                <span className="gray">{props.location}</span>
-            </div>
-            <p className="card--title">{props.title}</p>
-            <p className="card--title">{props.description}</p>
-            <p className="card--price">
-                <span className="bold">DKK {props.price}</span> / pr. person
-            </p>
-        </div> 
+        <div id="card">
+                <img 
+                    src={props.coverImg}
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                        <span className="gray">{props.location}</span>
+                    <h4 className="card--title">{props.title}</h4>
+                    <p className="card--title">{props.description}</p>
+                    <p className="card--price">
+                    <span className="bold">DKK {props.price}</span> / pr. person
+                    </p>
+                </div>
         </div> 
         </> 
     ); 
