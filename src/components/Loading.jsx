3d5b7aef47a9c5bb2731988e8224ae9505a28ed9
Rune <rune1833@live.dk>
2022-12-09 13:40:51 +0100

    Lading done måske

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import logo from "../assets/images/skiarenalogo.svg";
+/* import logo from "../assets/images/skiarenalogo.svg"; */
 
 export default function Loading() {
 	return (
 		<div className='loadingWrapper'>
-			<img className='loadingLogo' src={logo} alt='SkiArena logo' />
-			<p className="loadingText">Indlæser... Chill seriøst.</p>
+			<div className="logoText">
+				{/* <img className='loadingLogo' src={logo} alt='SkiArena logo' /> */}
+				<div class="loader"></div>
+				<p className="loadingText">Indlæser... Chill seriøst.</p>
+			</div>
 		</div>
 	);
 }
